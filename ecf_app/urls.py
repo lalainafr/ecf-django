af8d75df230e1e179cd195276794e33f236f92f5
rajaonahsoa <r.lalainafr@gmail.com>
2024-10-31 21:27:45 +0100

    design offer page

--- ecf_app/urls.py
+++ ecf_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('offer/', views.offer, name='offer'),
 ]
--- ecf_app/views.py
+++ ecf_app/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-
     return render(request, 'ecf_app/home.html')
+
+
+def offer(request):
+    return render(request, 'ecf_app/offer.html')

6f2a44eda102a0b62afb3fd85b30b78c1aae2647
rajaonahsoa <r.lalainafr@gmail.com>
2025-01-15 18:23:35 +0100

    deploy static file

--- ecf_django/settings.py
+++ ecf_django/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     # // Locale datetime
     'django.middleware.locale.LocaleMiddleware', 
@@ -134,6 +135,15 @@ USE_TZ = True
 # static file implementation
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = ['static/']
+
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # MEDIA FILE
     # URL - every time  we upload image it will go to MEDIA directory
